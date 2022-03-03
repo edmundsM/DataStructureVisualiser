5b688fb015027da28ddf4cb7d5e542022045b8ce
Maximilian Edmunds <edmunds.m@etoncollege.org.uk>
2022-03-03 11:52:40 +0000

    Initial Commit

---
+++ DataStructureVisualiser/DataStructureVisualiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataStructureVisualiserApp.swift
+//  DataStructureVisualiser
+//
+//  Created by Edmunds, Maximilian (NA) on 03/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct DataStructureVisualiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
